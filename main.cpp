7c377fa93c53b4fe32c76bbb0e88de2c59b52878
Charles Schiermeyer <matkatmusic@gmail.com>
2020-01-18 16:46:16 -0800

    Added Part1 Instructions

--- main.cpp
+++ main.cpp
@@ -1,7 +1,60 @@
+/*
+ Project 9: Part 1/1
+ Chapter 5 Part 7 Task
+
+ Create a branch named Part1
+ 
+1) read Instructions.cpp
+
+Make the following program work, which makes use of Variadic templates and Recursion
+
+Once it's working: 
+Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
+ 
+ Send me the the link to your repl.it in a DM on Slack
+
+ Wait for my code review.
+ */
+
 #include <iostream>
+#include <string>
+#include <typeinfo>
+
+struct Point
+{
+    Point(float _x, float _y) : x(_x), y(_y) { }
+    Point& multiply(float m)
+    {
+        x *= m;
+        y *= m;
+        return *this;
+    }
+    std::string toString() const
+    {
+        std::string str;
+        str +="Point { x: ";
+        str += std::to_string(x);
+        str += ", y: ";
+        str += std::to_string(y);
+        str += " }";
+        return str;
+    }
+private:
+    float x{0}, y{0};
+};
+
+template<typename Type>
+struct Wrapper
+{
+    Wrapper(Type&& t) : val(std::move(t)) 
+    { 
+        std::cout << "Wrapper(" << typeid(val).name() << ")" << std::endl; 
+    }
+};
 
 int main()
 {
-    std::cout << "good to go!" << std::endl;
-    return 0;    
+    variadicHelper( 3, std::string("burgers"), 2.5, Point{3.f, 0.14f} );
 }
+
+
